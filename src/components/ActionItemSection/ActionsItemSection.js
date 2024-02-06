96fa0dcd2a5fbf25fe77bcc508538c701f6f1e36
ananyagarg22 <garg.ananya2000@gmail.com>
2024-02-06 18:13:53 +0530

    Complete Project Upload

---
+++ src/components/ActionItemSection/ActionsItemSection.js
@@ -0,0 +1,92 @@
+import search from '../../assets/search.png';
+import up_arrow from '../../assets/up-arrow.png';
+import down_arrow from '../../assets/down-arrow.png';
+import './ActionsItemSection.css';
+import {ACTION_DATA} from './data.js';
+import {useState} from 'react';
+
+function ActionItem (props) {
+    const [open, setOPen] = useState(props.id === 0 ? true : false);
+    const toggle = () => {
+        setOPen(!open);
+      };
+  return (
+    <div id='toggle'>
+      <button onClick={toggle}>{props.title}<img src={open?up_arrow:down_arrow} alt='arrows' height={15} width={15}></img></button>
+      {open && 
+      <div id='content'>
+        <div id='title'>{props.title}</div>
+        <div id='amountandstartdate'>
+            <div>{props.amount}</div>
+            <div>{Intl.DateTimeFormat('en-US', {
+                year:'numeric',
+                month:'short',
+                day:'numeric',
+            }).format(Date.now())}</div>
+        </div>
+        <div id='description'>{props.children}</div>
+      </div>}
+    </div>
+  );
+}
+
+export function ActionItemSection () {
+    const[sampleData, setSampleData] = useState(ACTION_DATA);
+
+    function sortByDate (ACTION_DATA) {
+
+        const sortedData = [...ACTION_DATA].sort((a,b) => {
+            return Date.parse(a.startdate) > Date.parse(b.startdate) ? -1 : 1 
+        })
+        console.log(sortedData)
+        setSampleData(sortedData)
+    }
+    function sortByAmount(ACTION_DATA) {
+
+        const sortedData = [...ACTION_DATA].sort((a,b) => {
+            return a.amount > b.amount ? -1 : 1 
+        })
+        setSampleData(sortedData)
+    }
+
+    function sortBy(event) {
+        if (event.target.value === 'amount'){
+            sortByAmount(ACTION_DATA)
+        }
+        else if (event.target.value === 'recency'){
+            sortByDate(ACTION_DATA)
+        } 
+    }
+
+    return(
+        <div id='action-centre'>
+            <div id='header'>
+                <h5>Action Items</h5>
+                <select name="filter" id="filter" defaultValue='recency' onChange={sortBy}>
+                    <option value="amount">Amount</option>
+                    <option value="recency">Recency</option>
+                </select>
+            
+            </div>
+            <div id='searchbar'>
+                <img src = {search} alt='Search' height={15} width={15}></img>
+                <input name='search' type='text' placeholder="Search action items"></input>
+            </div>
+            {/* for i 1 to 10{
+                collapsible(title,amount,description,startdate)
+            } */}
+            <ul id='list'>    
+                {sampleData.map((action)=>{
+                    return(
+                    <ActionItem key={action.title}
+                    id={action.id}
+                    title={action.title} 
+                    amount={action.amount}
+                    startdate={action.startdate}>
+                        {action.description}
+                    </ActionItem>
+                )})}
+            </ul>
+        </div>
+    )
+ }
---
+++ src/components/ActionItemSection/data.js
@@ -0,0 +1,42 @@
+export const ACTION_DATA = [
+    {
+      id: 0,  
+      title: 'Truminds',
+      amount: '67K',
+      startdate: '2024-02-23T05:00:06.147801',
+      description:
+        "Truminds Software Systems is a Specialized Engineering Solutions company for the Digital and Connected World offering expertise in 5G, IoT, AI/ML",
+    },
+    {
+      id: 1,
+      title: 'Google',
+      amount: '78K',
+      startdate: '2024-01-23T05:00:06.147801',
+      description:
+        "Google LLC is an American multinational technology company focusing on artificial intelligence, online advertising, search engine technology, cloud computing, computer software, quantum computing, e-commerce, and consumer electronics",
+    },
+    {
+      id: 2,
+      title: 'Microsoft',
+      amount: '52K',
+      startdate: '2023-01-23T05:00:06.147801',
+      description:
+        "Microsoft Corporation is an American multinational technology corporation headquartered in Redmond, Washington. Microsoft's best-known software products are the Windows line of operating systems, the Microsoft 365 suite of productivity applications, and the Edge web browser.",
+    },
+    {
+      id: 3,
+      title: 'Amazon',
+      amount: '43K',
+      startdate: '2023-10-23T05:00:06.147801',
+      description:
+        "Amazon.com, Inc. is an American multinational technology company focusing on e-commerce, cloud computing, online advertising, digital streaming, and artificial intelligence. It is considered one of the Big Five American technology companies; the other four are Alphabet, Apple, Meta, and Microsoft.",
+    },
+    {
+      id: 4,
+      title: 'BearingPoint',
+      amount: '62K',
+      startdate: '2024-01-15T05:00:06.147801',
+      description:
+        "BearingPoint helps the Banking & Capital Markets sector meet challenges and capture new opportunities.",
+    }
+  ];
