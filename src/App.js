4674d90419c04e3ba03b2bd8de96e4cd9e789bd2
ananyagarg22 <garg.ananya2000@gmail.com>
2024-02-24 03:32:53 +0000

    Enabling multipage and routing in project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import './App.css';
-import {ActionItemSection} from './components/ActionItemSection/ActionsItemSection';
-import { LearningProgress } from './components/learningProgress/LearningProgress';
-import { EQ } from './components/eq/EQ';
-import { Dealstatus } from './components/dealstatus/Dealstatus';
-import { QScore } from './components/qscore/QScore';
-import { UpcomingMeetings } from './components/upcomingmeetings/UpcomingMeetings';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './pages/Home/Home.js';
+import Playlist from './pages/Playlist/Playlist.js';
+
+const routes = createBrowserRouter([
+  { path:'/', element: <Home/> },
+  { path:'/playlist', element: <Playlist/> },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <QScore/>
-      <EQ/>
-      <Dealstatus/>
-      <UpcomingMeetings/>
-      <LearningProgress/>
-      <div id='actionitems'><ActionItemSection/></div>
-      {/* <div id='actionitems'><ActionItemSection/></div>
-      <div id='actionitems'><ActionItemSection/></div> */}
-      {/* <div id='actionitems'><ActionItemSection/></div> */}
-      {/* <div id='actionitems'><ActionItemSection/></div> */}
-      {/* <div id='actionitems'><ActionItemSection/></div> */}
-    </div>
-  );
+  return (<RouterProvider router={routes}/>);
 }
 
 export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,33 @@
+import './Home.css';
+
+import {Link} from 'react-router-dom';
+
+import {ActionItemSection} from '../../components/ActionItemSection/ActionsItemSection.js';
+import { LearningProgress } from '../../components/learningProgress/LearningProgress';
+import { EQ } from '../../components/eq/EQ';
+import { Dealstatus } from '../../components/dealstatus/Dealstatus';
+import { QScore } from '../../components/qscore/QScore';
+import { UpcomingMeetings } from '../../components/upcomingmeetings/UpcomingMeetings';
+
+function Home() {
+  return (
+    <div className="Home">
+      <QScore/>
+      <EQ/>
+      <p>
+        <Link 
+          to="/playlist" 
+          style={{textDecoration: 'none', color: 'white'}}
+        > 
+          + Create Playlist
+        </Link>
+      </p>
+      <Dealstatus/>
+      <UpcomingMeetings/>
+      <LearningProgress/>
+      <div id='actionitems'><ActionItemSection/></div>
+    </div>
+  );
+}
+
+export default Home;
